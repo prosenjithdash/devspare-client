65207223c9a66584c8297b1f96a18d08611cd472
majednitol <majadrahman7@gmail.com>
2024-12-10 15:59:46 +0600

    update

--- js/devspare.js
+++ js/devspare.js
@@ -1,8 +1,8 @@
-// console.log('Devspare Client Side with JS.')
+// // console.log('Devspare Client Side with JS.')
 
-const loadData = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    const data = await res.json();
-    console.log(data)
-}
-loadData(data.data);
+// const loadData = async () => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     const data = await res.json();
+//     console.log(data)
+// }
+// loadData(data.data);
