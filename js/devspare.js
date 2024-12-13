48c0bbd080720e525e91916279a9b6b40aba15a1
majednitol <majadrahman7@gmail.com>
2024-12-13 15:51:10 +0600

    update

--- js/devspare.js
+++ js/devspare.js
@@ -6,3 +6,29 @@
 //     console.log(data)
 // }
 // loadData(data.data);
+
+
+async function getAllNotification  () {
+    try {
+        const accessToken = localStorage.getItem("accessToken");
+        const res = await fetch("http://localhost/devspare/backend/api/v1/notifications/notification.php", {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+
+
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+
+        const data = await res.json();
+        console.log("notification", data)
+
+        return data;
+    } catch (error) {
+        console.error("Error fetching data (Fetch):", error.message);
+        return [];
+    }
+};
